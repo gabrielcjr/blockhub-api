7223fbb3c16cbaeb42f9c75518188dbff45bd422
gacarneirojr <gacarneirojr@hotmail.com>
2021-11-05 14:44:16 -0300

    create project and associate controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectController } from './project/project.controller';
+import { ProjectService } from './project/project.service';
+import { AssociateController } from './associate/associate.controller';
+import { AssociateService } from './associate/associate.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProjectController, AssociateController],
+  providers: [AppService, ProjectService, AssociateService],
 })
 export class AppModule {}
